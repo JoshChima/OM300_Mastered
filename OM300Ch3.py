42ce3539b5fad86e5c7303ae5a95ee1a2564d294
JoshChima <45519648+JoshChima@users.noreply.github.com>
2019-02-13 22:37:07 -0600

    Okay!!!

--- OM300Ch3.py
+++ OM300Ch3.py
@@ -1,6 +1,6 @@
-name = []
-time = []
-Ip = []
+name = ['A','B','C','D','E','F','G']
+time = [3,5,1,10,4,5,10]
+Ip = [[False], [False], [False], ['B'], ['A','D'], ['C'], ['E','F']]
 Activities = []
 If = []
 Es = {}
@@ -12,6 +12,18 @@ def ActInput(name, time, Ip):
     Activity = {}
     Activity.update({'name':'%s'%(name), 'time':'%d'%(time), 'Ip': Ip})
     Activities.append(Activity)
+def MActInput(nlst, tlst, Iplst):
+    if len(tlst) != len(nlst):
+        print('RangeError, length of time does not match length of names')
+    elif len(Iplst) != len(nlst):
+                print('RangeError, length of Ip does not match length of names')
+    else:
+        for x in range(len(nlst)):
+            nx = nlst[x]
+            tx = tlst[x]
+            Ipx = Iplst[x]
+            ActInput(nx,tx,Ipx)
+
 
 ActInput('A', 3, ['B','C'])
 ActInput('B', 6, ['D'])
