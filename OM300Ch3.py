48b24b58c59d222e73b6775655c17632d1216543
JoshChima <45519648+JoshChima@users.noreply.github.com>
2019-02-13 22:23:00 -0600

    getting there

--- OM300Ch3.py
+++ OM300Ch3.py
@@ -1,14 +1,23 @@
-Activities = []
-time = {}
+name = []
+time = []
 Ip = []
+Activities = []
 If = []
 Es = {}
 Ef = {}
 Ls = {}
 Lf = {}
 
-def ActIn(name, time, Ip):
-    
+def ActInput(name, time, Ip):
+    Activity = {}
+    Activity.update({'name':'%s'%(name), 'time':'%d'%(time), 'Ip': Ip})
+    Activities.append(Activity)
+
+ActInput('A', 3, ['B','C'])
+ActInput('B', 6, ['D'])
+
+print(Activities)
+
 
 #test = [{"tname":3}, {"tname":6}, {"tname":8}]
 #def pick(x):
