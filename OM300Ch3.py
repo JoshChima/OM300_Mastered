2118cb98b5dc82db736da7d5db524206a0ddb4ce
JoshChima <45519648+JoshChima@users.noreply.github.com>
2019-02-14 21:23:03 -0600

    LET'S GOOOOOO!!!!

--- OM300Ch3.py
+++ OM300Ch3.py
@@ -13,27 +13,27 @@ Lf = {}
 def ActInput(name, time, Ip):
     Activity = {}
     Activity.update({'name':'%s'%(name), 'time':'%d'%(time), 'Ip': Ip})
-    timelib.update({'%s'%(name):'%d'%(time)})
+    timelib.update({'%s'%(name):time})
     Iplib.update({'%s'%(name): Ip})
     Activities.append(Activity)
 def ESoutput(Anum):
     if Iplib[Anum] == 'None':
         ES = 0
+        Es.update({'%s'%(Anum):ES})
     elif Iplib[Anum] != 'None' and len(Iplib[Anum]) == 1:
-        Ipnu = Iplib[Anum]
+        Ipnu = Iplib[Anum][0]
         IpEf = Ef[Ipnu]
-        ES = IpEf
+        Es.update({'%s'%(Anum):IpEf})
     else:
         IpEfs = []
         for k in Iplib[Anum]:
-            Ipnu = Iplib[Anum][k]
-            IpEf = Ef[Ipnu]
+            IpEf = Ef[k]
             IpEfs.append(IpEf)
         ES = max(IpEfs)
-    Es.update({'%s'%(Anum):'%d'%(ES)})
+        Es.update({'%s'%(Anum):ES})
 def EFoutput(Anum):
-    EF = Es[Anum] + timelib[Anum]
-    Ef.update({'%s'%(Anum):'%d'%(EF)})
+    EF = int(Es[Anum]) + int(timelib[Anum])
+    Ef.update({'%s'%(Anum):EF})
 
 def MActInput(nlst, tlst, Iplst):
     if len(tlst) != len(nlst):
@@ -49,14 +49,15 @@ def MActInput(nlst, tlst, Iplst):
             x += 1
             ActInput(nx,tx,Ipx)
 def Actmove1(nlst, tlst, Iplst):
-    MActInput(nlst, tlst, Iplst) 
+    MActInput(nlst, tlst, Iplst)
     for x in name:
         if Iplib[x] == 'None':
             ESoutput(x)
             EFoutput(x)
-        for y in Iplib[x]:
-            ESoutput(y)
-            EFoutput(y)
+        if Iplib[x] != 'None':
+            ESoutput(x)
+            EFoutput(x)
+
 #ActInput('A', 3, ['B','C'])
 #ActInput('B', 6, ['D'])
 
@@ -64,6 +65,9 @@ print(MActInput(name, time, Ip))
 print(Activities)
 print(timelib)
 print(Iplib)
+print(Actmove1(name, time, Ip))
+print(Es)
+print(Ef)
 
 #test = [{"tname":3}, {"tname":6}, {"tname":8}]
 #lndtest = {'T1':[3,4], 'T2':[8,6], 'T3':[7,9]}
