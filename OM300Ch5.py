88e32b0670cb97ab71c6f08a2aadf0eed3815964
JoshChima <45519648+JoshChima@users.noreply.github.com>
2019-02-25 21:35:10 -0600

    Ch5

---
+++ OM300Ch5.py
@@ -0,0 +1,9 @@
+class Total_Life_Cycle_Cost:
+    
+    def __init__(self, VPCost, VOcost, ULoV, MpY, MpG, AveFPpG):
+        self.VPCost = VPCost
+        self.VOcost = VOcost
+        self.ULoV = ULoV
+        self.MpY = MpY
+        self.MpG = MpG
+        self.AveFPpG = AveFPpG
