1304be816cfe9f44e00f4554e44f250d417ff551
JoshChima <45519648+JoshChima@users.noreply.github.com>
2019-02-26 23:34:56 -0600

    Crazy out here fam

--- OM300Ch5.py
+++ OM300Ch5.py
@@ -1,5 +1,7 @@
 import scipy as sy
 import sympy as sym
+from scipy.optimize import fsolve
+import numpy as np
 class Total_Life_Cycle_Cost:
     
     def __init__(self, VPCost, VOcost, ULoV, MpY, MpG, AveFPpG):
@@ -19,5 +21,32 @@ class Total_Life_Cycle_Cost:
         return float(self.VPCost) + float(self.LCCoF()) + float(self.LCOC())
 
 V1 = Total_Life_Cycle_Cost(18000, 0.15, 16, 15500, 31, 3.66)
-print(V1.TLCC())
+#print(V1.TLCC())
 
+V2 = Total_Life_Cycle_Cost(18000, 0.14, 15, 15500, 33, 3.77)
+V3 = Total_Life_Cycle_Cost(19500, 0.08, 15, 15500, 35, 3.77)
+
+M = np.array(range(0, 300))
+#VV2 = V2.VPCost + ((V2.AveFPpG/V2.MpG) + V2.VOcost) * M
+#VV3 = V3.VPCost + ((V3.AveFPpG/V3.MpG) + V3.VOcost) * M
+difl = []
+for m in range(len(M)):
+    x = M[m]
+    VV2 = (V2.AveFPpG/V2.MpG) + V2.VOcost
+    VV3 = (V3.AveFPpG/V3.MpG) + V3.VOcost
+    func1 = V2.VPCost + (float(x) * VV2)
+    func2 = V3.VPCost + (float(x) * VV3)
+    dif = abs(func1 - func2)
+    difl.append(dif)
+    if func1 == func2:
+        print(x)
+    #elif difl[m] == min(difl):
+        #print(difl[m])
+        #print(x)
+    else:
+        continue
+print(difl)
+print(min(difl))
+#VV23A = sym.geometry.in
+
+#print(VV23A)
